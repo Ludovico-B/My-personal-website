a0b489b4aa99f953bc933769835c63c46f90ba27
Ludovico Barberis <lubarbe01@gmail.com>
2026-02-08 10:50:10 +0100

    Add insight Vercel

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
