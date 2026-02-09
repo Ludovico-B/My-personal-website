cdbb120fa4341222d67d6aff48911890d920c933
Ludovico Barberis <lubarbe01@gmail.com>
2026-02-09 19:24:07 +0100

    Fix Bug
    Fix the conflict throw vercel/react and /next, white screen and not deploy

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
